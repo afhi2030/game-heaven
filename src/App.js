bb47afeb608fbf80b073f39f51431ab9231eb54d
vermouth <zardsi1224@gmail.com>
2021-02-23 09:06:14 +0900

    block 3.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
+import React from 'react'
+import { 
+  BrowserRouter as Router,
+  Link, 
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
+import Breakout from './Games/Breakout/Index';
+import './App.css';
 
 function App() {
   return (
+    <Router>
     <div>
-      <h1>!!!</h1>
+      <h1>Hello Mini Game Heaven</h1>
+      <nav>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/breakout">Breakout</Link></li>
+      </nav>
+
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/breakout"><Breakout /></Route>
+      </Switch>
     </div>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
